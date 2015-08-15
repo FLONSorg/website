7d3d71f0ffac79f412f13090bd00827a1e438f3a
andibraeu <ab@andi95.de>
2015-08-15 20:27:29 +0200

    added imprint

---
+++ imprint.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<title>FLONS API Imprint</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Free Libre Open Networks Imprint">
+<meta name="author" content="Andreas Bräu" >
+
+<!-- Le styles -->
+<link href="//flons.org/inc/css/flons.css" rel="stylesheet">
+<link href="//flons.org/inc/css/bootstrap.min.css" rel="stylesheet"> 
+</head><body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+          <h2>Legal Disclosure</h2>
+            Information in accordance with section 5 TMG<br/><br/>Foerderverein Freie Netzwerke e.V.<br/>Stephanstr. 10<br/>c/o Rabener/Rau<br/>10559 Berlin<br/><h3>Represented by</h3>Monic Meisel<br />Iris Rabener<br />J&uuml;rgen Neumann<br />Christian Heise<br/>
+          <h3>Contact</h3>
+            Telephone: +49 30 53014673<br/>E-Mail: <a href="mailto:mail@foerderverein.freie-netzwerke.de">mail@foerderverein.freie-netzwerke.de</a><br/>Internetaddress: <a href="http://foerderverein.freie-netzwerke.de" target="_blank">http://foerderverein.freie-netzwerke.de</a><br/>
+          <h3>Register entry</h3>Entry in  Vereinsregister<br/>Register Number: VR 22961<br/>Register Court: Amtsgericht Berlin Charlottenburg<br/>
+          <h3>Person responsible for content in accordance with 55 Abs. 2 RStV</h3>Dr. Olaf Koglin<br />
+Rechtsanwalt<br />
+An der Aussicht 40<br />
+13503 Berlin<br/>
+          <h3>Disclaimer</h3>
+            Accountability for content<br/>The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents' accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. In this context, please note that we are accordingly not obliged to monitor merely the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per &sect;&sect; 8 to 10 of the Telemedia Act (TMG).<br/><br/>Accountability for links<br/>Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective link immediately.<br/><br/>Copyright<br/> Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law (&sect; 44a et seq. of the copyright law), every form of utilizing, reproducing or processing works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights. Individual reproductions of a work are allowed only for private use, so must not serve either directly or indirectly for earnings. Unauthorized utilization of copyrighted works is punishable (&sect; 106 of the copyright law).<br/><br/><i>Quelle: </i><a href="http://www.twigg.de" target="_blank">twiggs translations</a><br/><br/>
+
+</div>
+    </div>
+</div>
+</body>
+</html>
